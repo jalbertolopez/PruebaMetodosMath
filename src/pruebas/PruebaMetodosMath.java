7630d36bd03e9c22125e35b189967ad3c35e9e92
jalbertolopez <jalbertolopez11@gmail.com>
2017-03-15 13:22:15 -0600

    Agrege constantes

--- src/pruebas/PruebaMetodosMath.java
+++ src/pruebas/PruebaMetodosMath.java
@@ -12,29 +12,34 @@ public class PruebaMetodosMath
     
     public static void main( String[] args )
     {
-       //Valor absoluto
-       System.out.println( "Math.abs( -45.8 ) = " + Math.abs( -45.8 ) );
        
-       //Redondeos y Truncamientos
-       System.out.println( "Math.ceil( 19.25 ) = " + Math.ceil( 19.25 ) );
-       System.out.println( "Math.ceil( -19.8 ) = " + Math.ceil( -19.8 ) );
-       System.out.println( "Math.floor( 4.2 ) = " + Math.floor( 4.2 ) );
-       System.out.println( "Math.floor( -3.8 ) = " + Math.floor( -3.8 ) );
-       
-       //Mayor o menor de 2 numeros
-       System.out.println( "Math.max( 4.6, 2.2 ) = " + Math.max( 4.6, 2.2 ) );
-       System.out.println( "Math.max( -4.6, -2.2 ) = " + Math.max( -4.6, -2.2 ) );
-       System.out.println( "Math.min( 4.6, 2.2 ) = " + Math.min( 4.6, 2.2 ) );
-       System.out.println( "Math.min( -4.6, -2.2 ) = " + Math.min( -4.6, -2.2 ) );
-       
-       //Trigonometricas
-       System.out.println( "Math.cos( 0 ) = " + Math.cos( 0 ) );
-       System.out.println( "Math.sin( 0 ) = " + Math.sin( 0 ) );
-       System.out.println( "Math.tan( 0 ) = " + Math.tan( 0 ) );
-       
-       //Potencia y raiz cuadrada
-       System.out.println( "Math.pow( 9, 2 ) = " + Math.pow( 9, 2 ) );
-       System.out.println( "Math.sqrt( 900 ) = " + Math.sqrt( 900 ) );
+        //Constantes
+        System.out.println( "Math.PI = " + Math.PI );
+        System.out.println( "Math.E = " + Math.E );
+        
+        //Valor absoluto
+        System.out.println( "Math.abs( -45.8 ) = " + Math.abs( -45.8 ) );
+
+        //Redondeos y Truncamientos
+        System.out.println( "Math.ceil( 19.25 ) = " + Math.ceil( 19.25 ) );
+        System.out.println( "Math.ceil( -19.8 ) = " + Math.ceil( -19.8 ) );
+        System.out.println( "Math.floor( 4.2 ) = " + Math.floor( 4.2 ) );
+        System.out.println( "Math.floor( -3.8 ) = " + Math.floor( -3.8 ) );
+
+        //Mayor o menor de 2 numeros
+        System.out.println( "Math.max( 4.6, 2.2 ) = " + Math.max( 4.6, 2.2 ) );
+        System.out.println( "Math.max( -4.6, -2.2 ) = " + Math.max( -4.6, -2.2 ) );
+        System.out.println( "Math.min( 4.6, 2.2 ) = " + Math.min( 4.6, 2.2 ) );
+        System.out.println( "Math.min( -4.6, -2.2 ) = " + Math.min( -4.6, -2.2 ) );
+
+        //Trigonometricas
+        System.out.println( "Math.cos( 0 ) = " + Math.cos( 0 ) );
+        System.out.println( "Math.sin( 0 ) = " + Math.sin( 0 ) );
+        System.out.println( "Math.tan( 0 ) = " + Math.tan( 0 ) );
+
+        //Potencia y raiz cuadrada
+        System.out.println( "Math.pow( 9, 2 ) = " + Math.pow( 9, 2 ) );
+        System.out.println( "Math.sqrt( 900 ) = " + Math.sqrt( 900 ) );
       
     }
    
